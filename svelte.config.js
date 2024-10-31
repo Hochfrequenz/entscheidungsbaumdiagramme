8047e05caaf75af6858f7b1945e81ac0f82f3cb7
olli <info@oliverlahr.de>
2024-10-31 16:32:26 +0100

    Fix vitePreprocess import

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import azure from "svelte-adapter-azure-swa";
 
 /** @type {import('@sveltejs/kit').Config} */
