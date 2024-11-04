96009761679cb45884621768c4f2d9d6f24e1940
olli <info@oliverlahr.de>
2024-11-04 21:27:01 +0100

    Create type definition for EBD metadata

---
+++ src/lib/types/metadata.ts
@@ -0,0 +1,15 @@
+export interface MetaData {
+  metadata: {
+    chapter: string; // Netznutzungsthema
+    ebd_code: string; // "E_0000"
+    ebd_name: string; // "1.1.1.: <ebd_code>_Prüfen, ob [...]"
+    role: string; // prüfende Rolle
+    section: string; // "1.1.: AD: [...]"
+    remark: string; // no table provided by BDEW: "Derzeit ist für diese Entscheidung kein Entscheidungsbaum notwendig, da keine Antwort gegeben wird."
+  };
+}
+
+export interface EbdNameExtended {
+  ebd_code: string;
+  ebd_name: string;
+}
