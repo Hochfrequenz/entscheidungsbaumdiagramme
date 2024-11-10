cb7eddd2d0eb7c712b9b561ab3524a598669a96d
olli <144932831+OLILHR@users.noreply.github.com>
2024-11-10 22:46:44 +0100

    feat: Add arrow buttons for faster EBD navigation (#105)
    * Add lucide icon library
    
    * Add arrow icon component
    
    * Add `ebd-navigation` component
    
    * Use `handleNavigation` to calculate EBD index of current select options
    
    * Add `appearance-none` to remove browser-specific styles of fv/ebd select
    
    * Replace nav button wrapper by seperate button elements
    
    * Refactor `ebdId` to `ebdListIndex`

---
+++ src/lib/components/features/ebd-navigation.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { base } from "$app/paths";
+  import type { EbdNameExtended } from "$lib/types/metadata";
+
+  import IconArrow from "../shared/icon-arrow.svelte";
+
+  export let currentEbds: EbdNameExtended[] = [];
+  export let currentFormatVersion: string = "";
+  export let selectedEbdCode: string = "";
+
+  $: currentIndex = currentEbds.findIndex(
+    (ebd) => ebd.ebd_code === selectedEbdCode,
+  );
+
+  $: isFirstEbd = currentIndex === 0;
+  $: isLastEbd = currentIndex === currentEbds.length - 1;
+
+  function handleNavigation(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const ebdListIndex = target.closest("svg")?.id;
+
+    if (
+      !ebdListIndex ||
+      !currentFormatVersion ||
+      !selectedEbdCode ||
+      !currentEbds.length
+    ) {
+      return;
+    }
+
+    if (currentIndex === -1) return;
+
+    // set boundaries to disable navigation once the first/last EBD is reached
+    if (ebdListIndex === "previousEbd" && isFirstEbd) return;
+    if (ebdListIndex === "nextEbd" && isLastEbd) return;
+
+    let newIndex: number;
+    if (ebdListIndex === "nextEbd") {
+      newIndex = currentIndex + 1;
+    } else if (ebdListIndex === "previousEbd") {
+      newIndex = currentIndex - 1;
+    } else {
+      return;
+    }
+
+    const newEbd = currentEbds[newIndex].ebd_code;
+    goto(`${base}/ebd/${currentFormatVersion}/${newEbd}`);
+  }
+</script>
+
+<div class="flex flex-col gap-1">
+  <button
+    on:click={handleNavigation}
+    class="cursor-pointer"
+    class:opacity-35={isLastEbd}
+  >
+    <IconArrow id="nextEbd" />
+  </button>
+  <button
+    on:click={handleNavigation}
+    class="cursor-pointer"
+    class:opacity-35={isFirstEbd}
+  >
+    <IconArrow orientation="rotate-180" id="previousEbd" />
+  </button>
+</div>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,6 +1,7 @@
 // shared components
 export { default as Footer } from "$lib/components/shared/footer.svelte";
 export { default as Header } from "$lib/components/shared/header.svelte";
+export { default as IconArrow } from "$lib/components/shared/icon-arrow.svelte";
 export { default as IconBack } from "$lib/components/shared/icon-back.svelte";
 export { default as IconHeart } from "$lib/components/shared/icon-heart.svelte";
 export { default as IconLogin } from "$lib/components/shared/icon-login.svelte";
@@ -14,5 +15,6 @@ export { default as ToolsBanner } from "$lib/components/shared/tools-banner.svel
 // features
 export { default as EbdFormHeader } from "$lib/components/features/ebd-form-header.svelte";
 export { default as EbdInput } from "$lib/components/features/ebd-input.svelte";
+export { default as EbdNavigation } from "$lib/components/features/ebd-navigation.svelte";
 export { default as ExportButton } from "$lib/components/features/export-button.svelte";
 export { default as FormatVersionSelect } from "$lib/components/features/format-version-select.svelte";
