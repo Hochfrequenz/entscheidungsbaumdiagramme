9b7f00081e91a6250d968b6f03b8408221fd4c3f
olli <144932831+OLILHR@users.noreply.github.com>
2025-01-17 17:29:58 +0100

    chore: add texts and descriptions from landing page mockup (#134)
    * Add texts and descriptions from landing page mockup
    
    * Add remaining corporate design colors
    
    * Split ad banner into separate cards
    
    * Bump submodule
    
    * Update landing page text
    
    * Change `edi@energy` to `bdew-mako`
    
    Co-authored-by: konstantin <konstantin.klein@hochfrequenz.de>
    
    * Fix heading
    
    * Hide ad banner for now
    
    ---------
    
    Co-authored-by: konstantin <konstantin.klein@hochfrequenz.de>

--- src/lib/components/shared/landingpage-banner.svelte
+++ src/lib/components/shared/landingpage-banner.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
-  import { cubicOut } from "svelte/easing";
-  import { fly } from "svelte/transition";
+  // import { cubicOut } from "svelte/easing";
+  // import { fly } from "svelte/transition";
 
   let visible = false;
 
@@ -12,23 +12,24 @@
 
 <div class="flex justify-evenly w-full p-5 pt-10">
   {#if visible}
+    <!--
     <div
-      class="flex flex-col items-center"
+      class="flex flex-col items-center rounded-lg bg-hf-ahbtabellen p-8 shadow-md w-1/5 aspect-square"
       in:fly={{ x: -500, duration: 1000, delay: 250, easing: cubicOut }}
     >
       <a
         href="https://ahb-tabellen.stage.hochfrequenz.de"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex flex-col items-center text-secondary hover:text-secondary-focus group"
+        class="flex flex-col items-center group"
       >
         <div
           class="transform transition-transform duration-300 ease-in-out group-hover:scale-110 flex flex-col items-center"
         >
-          <p class="text-xl text-current text-center">AHB Tabellen</p>
+          <p class="text-xl text-center">AHB Tabellen</p>
         </div>
       </a>
-      <p class="text-sm text-justify pt-4 px-40">
+      <p class="text-sm text-justify pt-4 overflow-auto">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
@@ -36,14 +37,14 @@
     </div>
 
     <div
-      class="flex flex-col items-center"
+      class="flex flex-col items-center rounded-lg bg-hf-bedingungsbaum p-8 shadow-md w-1/5 aspect-square"
       in:fly={{ x: -500, duration: 1000, delay: 500, easing: cubicOut }}
     >
       <a
         href="https://bedingungsbaum.hochfrequenz.de"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex flex-col items-center text-secondary hover:text-secondary-focus group"
+        class="flex flex-col items-center group"
       >
         <div
           class="transform transition-transform duration-300 ease-in-out group-hover:scale-110 flex flex-col items-center"
@@ -51,7 +52,7 @@
           <p class="text-xl text-current text-center">Bedingungsbaum</p>
         </div>
       </a>
-      <p class="text-sm text-justify pt-4 px-40">
+      <p class="text-sm text-justify pt-4 overflow-auto">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
@@ -59,26 +60,27 @@
     </div>
 
     <div
-      class="flex flex-col items-center"
+      class="flex flex-col items-center rounded-lg bg-hf-fristenkalender p-8 shadow-md w-1/5 aspect-square"
       in:fly={{ x: -500, duration: 1250, delay: 750, easing: cubicOut }}
     >
       <a
         href="https://hochfrequenz.de/#fristenkalender"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex flex-col items-center text-secondary hover:text-secondary-focus group"
+        class="flex flex-col items-center group"
       >
         <div
           class="transform transition-transform duration-300 ease-in-out group-hover:scale-110 flex flex-col items-center"
         >
-          <p class="text-xl text-current text-center">Fristenkalender</p>
+          <p class="text-xl text-center">Fristenkalender</p>
         </div>
       </a>
-      <p class="text-sm text-justify pt-4 px-40">
+      <p class="text-sm text-justify pt-4 overflow-auto">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
         voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
       </p>
     </div>
+  -->
   {/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,14 +26,38 @@
   </div>
 </div>
 
-<section class="bg-primary flex-grow flex items-center justify-center">
-  <div class="rounded-lg bg-tint p-12 shadow-md">
-    <h1 class="text-2xl pb-5">Entscheidungsbaumdiagramme</h1>
+<section class="bg-primary flex-grow flex items-center justify-center pt-40">
+  <div class="rounded-lg bg-tint p-12 shadow-md w-1/2">
     <h2
-      class="flex justify-center text-sm border-b border-secondary pb-3 mb-5 uppercase"
+      class="border-b border-secondary inline-block pb-[10px] text-2xl uppercase text-center w-full"
     >
-      - aber es sind actually Diagramme.
+      Entscheidungsbaumdiagramme - als Diagramme
     </h2>
+    <p class="text-base text-justify pt-[20px]">
+      Wer mit den vom BDEW veröffentlichten Entscheidungsbaumdiagrammen (EBDs)
+      arbeitet weiß: Die tabellarische Form ist sperrig und mindestens
+      unübersichtlich. Unsere automatisch generierten EBDs von Hochfrequenz sind
+      hingegen echte Diagramme - lesbar und nutzungsfreundlich.
+    </p>
+    <p class="text-base text-justify pt-[20px]">
+      Die auf bdew-mako.de veröffentlichten Word-Dateien werden von uns
+      vollautomatisch ausgelesen. Aus den so gewonnenen Rohdaten erstellt unsere
+      Software anschließend ebenso automatisiert Diagramme, welche inhaltlich zu
+      100% den offiziellen BDEW-Dokumenten entsprechen. Diese Diagramme sollen
+      Fachbereichen und Menschen mit Prozess-Expertise dabei unterstützen, ihre
+      Systeme fit für neue oder angepasste Prüflogiken zu machen. Auf Grundlage
+      echter Diagramme lässt sich viel leichter überblicken, an welcher Stelle
+      Implementierungsaufwände anfallen, ohne dabei den Gesamtprozess aus den
+      Augen zu verlieren.
+    </p>
+    <p class="text-base text-justify pt-[20px]">
+      Die Diagramme können entweder als SVG-Dateien heruntergeladen werden, oder
+      auch als puml, um entsprechend eigene Erweiterungen, Notizen usw. darin
+      abzubilden.
+    </p>
+    <p
+      class="flex justify-center text-sm border-secondary pb-3 mb-5 uppercase"
+    ></p>
     <div class="mt-10 flex justify-center">
       <button
         on:click={checkAuthentication}
@@ -44,6 +68,6 @@
     </div>
   </div>
 </section>
-<section class="pb-8">
+<section class="pb-8 bg-primary">
   <LandingpageBanner />
 </section>
