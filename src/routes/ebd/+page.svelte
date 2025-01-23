13ff5f56d366b10dd9f23b1ad5051a8a47d7e54b
konstantin <konstantin.klein@hochfrequenz.de>
2025-01-23 10:23:18 +0100

    feat: Set background colour of div that contains the EBD (off-white) (#188)
    * feat: Set background colour of div that contains the EBD
    
    to the same as the SVG background
    
    * fix: switch from pastell blau to off-white
    
    to align with upcoming EBD background: https://github.com/Hochfrequenz/rebdhuhn/pull/337
    
    * prettier
    
    ---------
    
    Co-authored-by: Konstantin <konstantin.klein+github@hochfrequenz.de>

--- src/routes/ebd/+page.svelte
+++ src/routes/ebd/+page.svelte
@@ -216,7 +216,7 @@
     </svelte:fragment>
   </Header>
 
-  <div class="flex-1 overflow-hidden">
+  <div class="flex-1 overflow-hidden bg-tertiary">
     <SvgContainer {svgContent} {isLoading} {error} />
   </div>
 </div>
