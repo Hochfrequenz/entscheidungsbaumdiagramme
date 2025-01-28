bea5b2c25259d2f27fd60fd9b0c22a57d658d488
konstantin <konstantin.klein@hochfrequenz.de>
2025-01-28 08:19:30 +0100

    feat: add solution footer above existing footer (#205)
    ---------
    
    Co-authored-by: Konstantin <konstantin.klein+github@hochfrequenz.de>

--- src/lib/components/shared/footer.svelte
+++ src/lib/components/shared/footer.svelte
@@ -2,12 +2,80 @@
   import { base } from "$app/paths";
   import { IconHeart, IconLogo } from "$lib/components";
 
-  export let background = "bg-secondary";
-
   let currentYear = new Date().getFullYear();
 </script>
 
-<footer class="flex justify-between items-center {background} px-6 py-2.5">
+<footer class="flex items-center bg-tint px-3 py-3">
+  <div class="flex-1 flex justify-center">
+    <div class="flex items-center text-sm text-black/70 gap-10 text-center">
+      <a
+        href="https://ahb-tabellen.hochfrequenz.de"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex flex-row items-center rounded-full bg-ahb_primary text-white text-lg px-5 py-1
+            shadow-lg
+            ring-1
+            ring-black/5
+            hover:ring-black/10
+            transition-all
+            duration-300
+            ease-in-out
+            hover:scale-110"
+      >
+        AHB-Tabellen
+      </a>
+      <a
+        href="https://bedingungsbaum.hochfrequenz.de"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex flex-row items-center rounded-full bg-bedingungsbaum_primary text-white text-lg px-5 py-1
+            shadow-lg
+            ring-1
+            ring-black/5
+            hover:ring-black/10
+            transition-all
+            duration-300
+            ease-in-out
+            hover:scale-110"
+      >
+        Bedingungsbaum
+      </a>
+      <a
+        href="https://ebd.hochfrequenz.de"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex flex-row items-center rounded-full bg-ebd_primary text-white text-lg px-5 py-1
+            shadow-lg
+            ring-1
+            ring-black/5
+            hover:ring-black/10
+            transition-all
+            duration-300
+            ease-in-out
+            hover:scale-110"
+      >
+        Entscheidungsbaumdiagramme
+      </a>
+      <a
+        href="https://fristenkalender.hochfrequenz.de"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex flex-row items-center rounded-full bg-fristenkalender_primary text-white text-lg px-5 py-1
+            shadow-lg
+            ring-1
+            ring-black/5
+            hover:ring-black/10
+            transition-all
+            duration-300
+            ease-in-out
+            hover:scale-110"
+      >
+        Fristenkalender
+      </a>
+    </div>
+  </div>
+</footer>
+<footer class="flex justify-between items-center bg-secondary px-6 py-2.5">
   <a href="{base}/" title="landingpage" class="flex-none items-center">
     <IconLogo size={24} />
   </a>
