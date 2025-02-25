282d998b1826a6b35379717dfefaa9f60ecf9760
olli <144932831+OLILHR@users.noreply.github.com>
2025-02-25 20:52:25 +0100

    chore: replace download icon with custom HF download icon (#290)
    Replace download icon with custom HF download icon

--- src/lib/components/shared/icon-download.svelte
+++ src/lib/components/shared/icon-download.svelte
@@ -1,15 +1,28 @@
 <svg
+  viewBox="0 0 39.240002 41.66"
+  width="18"
+  height="18"
   xmlns="http://www.w3.org/2000/svg"
-  width="21"
-  height="21"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="currentColor"
-  stroke-width="2"
-  stroke-linecap="round"
-  stroke-linejoin="round"
-  class="lucide lucide-download"
-  ><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline
-    points="7 10 12 15 17 10"
-  /><line x1="12" x2="12" y1="15" y2="3" /></svg
 >
+  <defs>
+    <style>
+      .cls-1 {
+        stroke-linejoin: round;
+      }
+      .cls-1,
+      .cls-2 {
+        fill: none;
+        stroke: currentColor;
+        stroke-linecap: round;
+        stroke-width: 4px;
+      }
+      .cls-2 {
+        stroke-miterlimit: 10;
+      }
+    </style>
+  </defs>
+  <path class="cls-1" d="M37.24,39.66H2" />
+  <path class="cls-2" d="M19.62,2V28.34" />
+  <path class="cls-2" d="m29.37,21.08-9.75,7.25" />
+  <path class="cls-2" d="m9.87,21.08 9.75,7.25" />
+</svg>
