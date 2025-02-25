3c9efa68a89f581acaa69b6e37d7d9f422c552a5
olli <info@oliverlahr.de>
2025-02-25 21:18:33 +0100

    Import `logout` icon from companystylesheets submodule

--- src/lib/components/shared/icon-logout.svelte
+++ src/lib/components/shared/icon-logout.svelte
@@ -1,28 +1,15 @@
-<svg
-  viewBox="0 0 39.950001 45.039997"
-  width="16"
-  height="16"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <defs>
-    <style>
-      .cls-1 {
-        stroke-linejoin: round;
-      }
-      .cls-1,
-      .cls-2 {
-        fill: none;
-        stroke: currentColor;
-        stroke-linecap: round;
-        stroke-width: 4px;
-      }
-      .cls-2 {
-        stroke-miterlimit: 10;
-      }
-    </style>
-  </defs>
-  <path class="cls-2" d="M11.61,22.52H37.95" />
-  <path class="cls-2" d="m30.7,12.77 7.25,9.75" />
-  <path class="cls-2" d="m30.7,32.27 7.25,-9.75" />
-  <path class="cls-1" d="M16.4,42.96 2,43.04V2c0,0 14.9,0 14.9,0" />
-</svg>
+<script lang="ts">
+  import { base } from "$app/paths";
+
+  export let width = "16";
+  export let height = "16";
+  export let stroke = "rgb(0 0 0 / 0.7)";
+</script>
+
+<img
+  src="{base}/companystylesheet/icons/logout.svg"
+  {width}
+  {height}
+  alt="Logout icon"
+  style="stroke: {stroke}"
+/>
