119ff627fe11a49a77b097b6428d6cf3fc0f66d1
olli <144932831+OLILHR@users.noreply.github.com>
2025-02-25 22:58:50 +0100

    feat: auto select format version by default depending on current date (#294)
    * Select format version by default depending on current date
    
    * Remove obsolete css classes from icons

--- src/lib/components/shared/icon-download.svelte
+++ src/lib/components/shared/icon-download.svelte
@@ -1,28 +1,15 @@
-<svg
-  viewBox="0 0 39.240002 41.66"
-  width="18"
-  height="18"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <defs>
-    <style>
-      .cls-1 {
-        stroke-linejoin: round;
-      }
-      .cls-1,
-      .cls-2 {
-        fill: none;
-        stroke: currentColor;
-        stroke-linecap: round;
-        stroke-width: 4px;
-      }
-      .cls-2 {
-        stroke-miterlimit: 10;
-      }
-    </style>
-  </defs>
-  <path class="cls-1" d="M37.24,39.66H2" />
-  <path class="cls-2" d="M19.62,2V28.34" />
-  <path class="cls-2" d="m29.37,21.08-9.75,7.25" />
-  <path class="cls-2" d="m9.87,21.08 9.75,7.25" />
-</svg>
+<script lang="ts">
+  import { base } from "$app/paths";
+
+  export let width = "18";
+  export let height = "18";
+  export let stroke = "rgb(0 0 0 / 0.7)";
+</script>
+
+<img
+  src="{base}/companystylesheet/icons/download.svg"
+  alt="Exportieren"
+  {width}
+  {height}
+  style="stroke: {stroke}"
+/>
