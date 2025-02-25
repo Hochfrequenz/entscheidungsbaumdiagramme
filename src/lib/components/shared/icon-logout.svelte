04f7113b5cfbfa6df04a0db6ed7d278bfa4a7d8a
olli <info@oliverlahr.de>
2025-02-25 21:01:49 +0100

    Replace logout icon with custom HF logout icon

--- src/lib/components/shared/icon-logout.svelte
+++ src/lib/components/shared/icon-logout.svelte
@@ -1,15 +1,28 @@
 <svg
+  viewBox="0 0 39.950001 45.039997"
+  width="16"
+  height="16"
   xmlns="http://www.w3.org/2000/svg"
-  width="22"
-  height="22"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="currentColor"
-  stroke-width="2"
-  stroke-linecap="round"
-  stroke-linejoin="round"
-  class="lucide lucide-log-out"
-  ><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline
-    points="16 17 21 12 16 7"
-  /><line x1="21" x2="9" y1="12" y2="12" /></svg
 >
+  <defs>
+    <style>
+      .cls-1 {
+        stroke-linejoin: round;
+      }
+      .cls-1,
+      .cls-2 {
+        fill: none;
+        stroke: currentColor;
+        stroke-linecap: round;
+        stroke-width: 4px;
+      }
+      .cls-2 {
+        stroke-miterlimit: 10;
+      }
+    </style>
+  </defs>
+  <path class="cls-2" d="M11.61,22.52H37.95" />
+  <path class="cls-2" d="m30.7,12.77 7.25,9.75" />
+  <path class="cls-2" d="m30.7,32.27 7.25,-9.75" />
+  <path class="cls-1" d="M16.4,42.96 2,43.04V2c0,0 14.9,0 14.9,0" />
+</svg>
