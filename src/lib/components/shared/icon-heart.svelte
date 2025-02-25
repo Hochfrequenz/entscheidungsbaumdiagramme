27faf2a006ee110962c5df32a86b2e0062b103f8
olli <info@oliverlahr.de>
2025-02-25 22:55:43 +0100

    Remove obsolete css classes from icons

--- src/lib/components/shared/icon-heart.svelte
+++ src/lib/components/shared/icon-heart.svelte
@@ -1,65 +1,15 @@
-<svg
-  viewBox="0 0 43.759998 42.867897"
-  width="16"
-  height="16"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <defs>
-    <style>
-      .cls-1,
-      .cls-2 {
-        fill: none;
-      }
-      .cls-3 {
-        clip-path: url(#clippath-1);
-      }
-      .cls-2 {
-        stroke: currentColor;
-        stroke-linecap: round;
-        stroke-miterlimit: 10;
-        stroke-width: 4px;
-      }
-      .cls-4 {
-        clip-path: url(#clippath);
-      }
-    </style>
-    <clipPath id="clippath">
-      <rect
-        class="cls-1"
-        x="13.12"
-        y="13.56"
-        width="21.879999"
-        height="42.869999"
-      />
-    </clipPath>
-    <clipPath id="clippath-1">
-      <rect
-        class="cls-1"
-        x="35"
-        y="13.56"
-        width="21.879999"
-        height="42.869999"
-      />
-    </clipPath>
-  </defs>
-  <g
-    class="cls-4"
-    clip-path="url(#clippath)"
-    transform="translate(-13.12,-13.562102)"
-  >
-    <path
-      class="cls-2"
-      d="m 35.33,54.44 c 0,0 -22.6,-13.13 -20,-29.7 0.89,-5.67 5.54,-9.47 10.36,-9.16 6.1,0.39 9.3,7.56 9.3,7.56"
-    />
-  </g>
-  <g
-    class="cls-3"
-    clip-path="url(#clippath-1)"
-    transform="translate(-13.12,-13.562102)"
-  >
-    <path
-      class="cls-2"
-      d="m 34.67,54.44 c 0,0 22.6,-13.13 20,-29.7 -0.89,-5.67 -5.54,-9.47 -10.36,-9.16 -6.1,0.39 -9.3,7.56 -9.3,7.56"
-    />
-  </g>
-</svg>
+<script lang="ts">
+  import { base } from "$app/paths";
+
+  export let width = "16";
+  export let height = "16";
+  export let stroke = "rgb(0 0 0 / 0.7)";
+</script>
+
+<img
+  src="{base}/companystylesheet/icons/herz.svg"
+  alt="Herz"
+  {width}
+  {height}
+  style="stroke: {stroke}"
+/>
--- src/lib/components/shared/icon-login.svelte
+++ src/lib/components/shared/icon-login.svelte
@@ -1,28 +1,45 @@
+<script lang="ts">
+  export let width = "18";
+  export let height = "18";
+  export let stroke = "#FFFFFF";
+</script>
+
 <svg
+  {width}
+  {height}
   viewBox="0 0 44.67 45.04"
-  width="18"
-  height="18"
   xmlns="http://www.w3.org/2000/svg"
 >
-  <defs>
-    <style>
-      .cls-1 {
-        stroke-linejoin: round;
-      }
-      .cls-1,
-      .cls-2 {
-        fill: none;
-        stroke: currentColor;
-        stroke-linecap: round;
-        stroke-width: 4px;
-      }
-      .cls-2 {
-        stroke-miterlimit: 10;
-      }
-    </style>
-  </defs>
-  <path class="cls-2" d="M2,22.52H28.34" />
-  <path class="cls-2" d="m21.08,12.77 7.25,9.75" />
-  <path class="cls-2" d="m21.08,32.27 7.25,-9.75" />
-  <path class="cls-1" d="m28.27,2.08 14.4,-0.08V43.04c0,0-14.9,0-14.9,0" />
+  <path
+    d="M2 22.52H28.34"
+    fill="none"
+    {stroke}
+    stroke-linecap="round"
+    stroke-width="4"
+    stroke-miterlimit="10"
+  />
+  <path
+    d="m21.08 12.77 7.25 9.75"
+    fill="none"
+    {stroke}
+    stroke-linecap="round"
+    stroke-width="4"
+    stroke-miterlimit="10"
+  />
+  <path
+    d="m21.08 32.27 7.25-9.75"
+    fill="none"
+    {stroke}
+    stroke-linecap="round"
+    stroke-width="4"
+    stroke-miterlimit="10"
+  />
+  <path
+    d="m28.27 2.08 14.4-0.08V43.04H27.77"
+    fill="none"
+    {stroke}
+    stroke-linecap="round"
+    stroke-width="4"
+    stroke-linejoin="round"
+  />
 </svg>
