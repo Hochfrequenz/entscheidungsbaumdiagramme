b57ffa1091b53ae38b43db0975bee75cb818f711
olli <info@oliverlahr.de>
2025-03-07 13:18:56 +0100

    Hide button if no PUML file can be found

--- src/lib/components/features/export-puml-button.svelte
+++ src/lib/components/features/export-puml-button.svelte
@@ -12,12 +12,35 @@
   let cooldownTimer: ReturnType<typeof setTimeout>;
   let remainingSeconds = 0;
   let downloadStarted = false;
+  let isPumlAvailable = false;
 
   onMount(() => {
     return () => {
       if (cooldownTimer) clearTimeout(cooldownTimer);
     };
   });
+
+  // check if PUML file is availabe (roughly 75% of SVGs have an associated PUML file)
+  $: if (currentFormatVersion && currentEbd) {
+    checkPumlAvailability();
+  }
+
+  async function checkPumlAvailability() {
+    if (!currentFormatVersion || !currentEbd) {
+      isPumlAvailable = false;
+      return;
+    }
+
+    const pumlPath = `${base}/ebd/${currentFormatVersion}/${currentEbd}.puml`;
+
+    try {
+      const response = await fetch(pumlPath, { method: "HEAD" });
+      isPumlAvailable = response.ok;
+    } catch (err) {
+      console.error(`Error checking PUML availability: ${err}`);
+      isPumlAvailable = false;
+    }
+  }
 
   // 5 seconds cooldown to prevent download from being spammed
   function startCooldownTimer() {
@@ -38,7 +61,13 @@
   }
 
   async function handleExport() {
-    if (isDisabled || !isExportReady || !currentFormatVersion || !currentEbd)
+    if (
+      isDisabled ||
+      !isExportReady ||
+      !currentFormatVersion ||
+      !currentEbd ||
+      !isPumlAvailable
+    )
       return;
 
     isExportReady = false;
@@ -69,16 +98,18 @@
   }
 </script>
 
-<button
-  on:click={handleExport}
-  class="flex flex-row items-center gap-2 rounded-full bg-secondary text-[16px] py-3 px-5 text-black/70 transition-opacity duration-200 whitespace-nowrap min-w-[150px] justify-center"
-  class:opacity-30={isDisabled || !isExportReady}
-  class:cursor-not-allowed={!isExportReady}
-  disabled={isDisabled || !isExportReady}
->
-  {#if downloadStarted}
-    {remainingSeconds} s
-  {:else}
-    <IconDownload /> Export PUML
-  {/if}
-</button>
+{#if isPumlAvailable}
+  <button
+    on:click={handleExport}
+    class="flex flex-row items-center gap-2 rounded-full bg-secondary text-[16px] py-3 px-5 text-black/70 transition-opacity duration-200 whitespace-nowrap min-w-[150px] justify-center"
+    class:opacity-30={isDisabled || !isExportReady}
+    class:cursor-not-allowed={!isExportReady}
+    disabled={isDisabled || !isExportReady}
+  >
+    {#if downloadStarted}
+      {remainingSeconds} s
+    {:else}
+      <IconDownload /> Export PUML
+    {/if}
+  </button>
+{/if}
